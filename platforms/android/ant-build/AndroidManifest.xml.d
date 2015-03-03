cbe791237f806f317f9378ae1a796ae12264f88d
Eileen <user12@LAB12.WebDev.class>
2015-03-03 13:38:40 -0700

    Initial Add of ch03 project.

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\sandbox\webd265\ch03\platforms\android\ant-build\AndroidManifest.xml : \
+c:\sandbox\webd265\ch03\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,12 @@
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes.dex : \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\ch03\BuildConfig.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\ch03\CordovaApp.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\ch03\R$attr.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\ch03\R$drawable.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\ch03\R$string.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\ch03\R$xml.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\ch03\R.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\org\apache\cordova\globalization\Globalization$1.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\org\apache\cordova\globalization\Globalization.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\classes\org\apache\cordova\globalization\GlobalizationError.class \
+c:\sandbox\webd265\ch03\platforms\android\ant-build\dexedLibs\classes-7a801128818f39e9b583c95f926e3b43.jar \
